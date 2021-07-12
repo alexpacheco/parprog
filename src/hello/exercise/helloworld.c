40cd70c7cb672231ea74f99224035e9d4a39f1a1
Alex Pacheco <abpacheco@gmail.com>
2021-07-12 08:21:19 -0400

    First draft for 2021 Workshop

--- src/hello/exercise/helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/* Include omp.h ? */
-int main() {
-  #pragma omp parallel
-  {
-    printf("Hello world from thread %d out of %d threads\n",
-	   omp_get_thread_num(), omp_get_thread_num());
-  }
-}
--- src/hello/solution/helloworld.f90
+++
@@ -1,9 +0,0 @@
-program hello
-
-  implicit none
-
-  !$omp parallel
-  print *, 'Hello World'
-  !$omp end parallel
-
-end program hello
